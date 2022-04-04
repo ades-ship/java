e6eb35b92e61e7645c885147892fb56e5ed7434c
adesh sengar <81346064+ades-ship@users.noreply.github.com>
2022-04-05 01:06:15 +0530

    upload first java file

---
+++ adesh.java
@@ -0,0 +1,25 @@
+class Hello2{
+  public static void main(String []args){
+   System.out.println("hello adesh");
+  }
+  public static void main(){
+   System.out.println("hello nitish");
+}
+}
+
+ class nitish{
+	   public static void main(String []args){
+		   int a=0;
+		   float b=10.5f,c=90.0f,d;
+		   d=b+c;
+		    System.out.println(d);
+		   System.out.println(a);
+		   if(true)
+		   {
+			     System.out.println(a);
+				 }
+		   
+   System.out.println("hello nitish");
+   
+  }
+  }
